7820ef16b95700432bb99940ad4aec81b58bd793
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2025-10-13 23:49:37 -0400

    add variable expansion for dev server command

--- app/src/dev_server.rs
+++ app/src/dev_server.rs
@@ -1,25 +1,33 @@
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 use anyhow::{Context, Result};
 use tokio_util::sync::CancellationToken;
 
 use crate::config::Config;
-use crate::expansion::expand_path;
+use crate::expansion::{expand_path, shell_expand};
 
 pub async fn run_dev_server(config: &Config, shutdown_token: CancellationToken) -> Result<()> {
     let dev_command = &config.develop_command;
     let app_src = PathBuf::from(expand_path(config.app_src.as_ref().context(
             "`app_src` must be provided either in your config file or as a CLI argument (neither was provided)"
         )?)?);
-    let mut dev_server = tokio::process::Command::new(
+    let args = HashMap::new();
+    let mut dev_server = tokio::process::Command::new(shell_expand(
         dev_command
             .first()
             .context("`develop_command` should have at least one part")?,
+        &args,
+    )?)
+    .args(
+        dev_command
+            .iter()
+            .map(|c| shell_expand(c, &args))
+            .collect::<Result<Vec<_>>>()?,
     )
     .current_dir(app_src)
     .env("API_PORT", &config.port)
     .env("DEV_SERVER_PORT", &config.dev_server_port)
-    .args(dev_command.iter().skip(1))
     .kill_on_drop(true)
     .spawn()
     .context("Problem starting dev server")?;
