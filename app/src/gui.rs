c6573d0f086f713f51579e9f951ae8510a0d7d04
Maddison Hellstrom <maddy@na.ai>
2025-10-16 21:48:43 -0700

    feat: add gtk layer shell support for wayland
    Note: for some reason, when using gtk-layer-shell, the `--window-opaque` flag causes the window to never show up.
    
    See: https://github.com/tauri-apps/tauri/issues/2083#issuecomment-2424150009

--- app/src/gui.rs
+++ app/src/gui.rs
@@ -1,4 +1,6 @@
+use anyhow::anyhow;
 use tao::{
+    dpi::PhysicalSize,
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
@@ -11,6 +13,54 @@ use crate::config::Config;
 #[derive(Debug, Clone)]
 pub enum AppEvent {
     Shutdown,
+}
+
+#[cfg(target_os = "linux")]
+fn is_wayland_window(window: &tao::window::Window) -> bool {
+    use gtk::{glib::ObjectExt, traits::WidgetExt};
+    use tao::platform::unix::WindowExtUnix;
+    window.gtk_window().display().type_().name() == "GdkWaylandDisplay"
+}
+
+#[cfg(target_os = "linux")]
+fn setup_wayland_layer_shell(
+    window: &tao::window::Window,
+    vbox: &gtk::Box,
+    size: Option<PhysicalSize<u32>>,
+) -> anyhow::Result<gtk::ApplicationWindow> {
+    use gtk::prelude::{ContainerExt, GtkWindowExt, WidgetExt};
+    use gtk_layer_shell::LayerShell;
+    use tao::platform::unix::WindowExtUnix;
+
+    let gtk_window = window.gtk_window();
+
+    // Hide the original tao window
+    gtk_window.hide();
+
+    let app = gtk_window
+        .application()
+        .ok_or_else(|| anyhow!("GTK application not available"))?;
+
+    // Create a new gtk window for the overlay
+    let overlay_window = gtk::ApplicationWindow::new(&app);
+
+    // To prevent the window from being black initially.
+    overlay_window.set_app_paintable(true);
+
+    // Move vbox to overlay window
+    gtk_window.remove(vbox);
+    overlay_window.add(vbox);
+
+    overlay_window.init_layer_shell();
+    overlay_window.set_layer(gtk_layer_shell::Layer::Overlay);
+    overlay_window.set_keyboard_interactivity(true);
+
+    if let Some(size) = size {
+        overlay_window.set_size_request(size.width as i32, size.height as i32);
+    }
+
+    overlay_window.show_all();
+    Ok(overlay_window)
 }
 
 pub fn run_gui(
@@ -38,11 +88,15 @@ pub fn run_gui(
     let _webview = builder.build(&window)?;
 
     #[cfg(target_os = "linux")]
-    let _webview = {
+    let (_webview, _overlay_window) = {
         use tao::platform::unix::WindowExtUnix;
         use wry::WebViewBuilderExtUnix;
         let vbox = window.default_vbox().unwrap();
-        builder.build_gtk(vbox)?
+        let webview = builder.build_gtk(vbox)?;
+        let overlay_window = is_wayland_window(&window)
+            .then(|| setup_wayland_layer_shell(&window, vbox, config.window.size()))
+            .transpose()?;
+        (webview, overlay_window)
     };
 
     event_loop.run(move |event, _, control_flow| {
